41f9472e44bbd0055f6d181936ec891dfaf8f831
yahya <yahiabahhous@gmail.com>
2024-06-24 22:50:42 +0300

    Add SQL query to create table for APP

---
+++ src/Util/Database.sql
@@ -0,0 +1,7 @@
+CREATE TABLE posts(
+    id INT NOT NULL AUTO_INCREMENT,
+    title varchar(255) NOT NULL,
+    content varchar(255) NOT NULL,
+    author varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+);
